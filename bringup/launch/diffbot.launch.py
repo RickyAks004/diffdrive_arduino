24670337c56347ceaf9544c947a4f3980a51e099
Ayush Kumar Sahu <122040973+RickyAks004@users.noreply.github.com>
2024-06-25 16:48:48 +0530

    Update diffbot.launch.py

--- bringup/launch/diffbot.launch.py
+++ bringup/launch/diffbot.launch.py
@@ -98,7 +98,7 @@ def generate_launch_description():
 
     nodes = [
         control_node,
-        robot_state_pub_node,
+        robot_state_pub_node, #look for it change it at the end.
         joint_state_broadcaster_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
